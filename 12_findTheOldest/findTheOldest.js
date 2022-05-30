7bc6d42d53541af3c69ddd6bbd76ca737bff72a3
LittleArmstrong <59065008+LittleArmstrong@users.noreply.github.com>
2022-05-30 15:16:41 +0200

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (persons) {
+   return persons.reduce((acc, cur) => {
+      const first = (acc?.yearOfDeath || new Date().getFullYear()) - acc.yearOfBirth;
+      const second = (cur?.yearOfDeath || new Date().getFullYear()) - cur.yearOfBirth;
+      return first > second ? acc : cur;
+   });
 };
 
 // Do not edit below this line

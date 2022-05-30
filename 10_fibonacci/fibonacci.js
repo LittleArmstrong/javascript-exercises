d46048990984b76a925b61013ea167770a8259f6
LittleArmstrong <59065008+LittleArmstrong@users.noreply.github.com>
2022-05-30 15:00:17 +0200

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (val) {
+   const num = Number(val);
+   if (num <= 0) return "OOPS";
+   if (num === 1 || num === 2) return 1;
+   return fibonacci(num - 1) + fibonacci(num - 2);
 };
+
+console.log(fibonacci(3));
 
 // Do not edit below this line
 module.exports = fibonacci;

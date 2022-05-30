9b2bdc572b2c8c8e54caba1ea923ab20bc2a6f0a
LittleArmstrong <59065008+LittleArmstrong@users.noreply.github.com>
2022-05-30 14:43:25 +0200

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+   const array = str.match(/[a-zA-Z]/g);
+   const l_array = array.map((char) => char.toLowerCase());
+   const reversed = [...l_array].reverse();
+   return l_array.every((val, i) => val === reversed[i]);
 };
+
+console.log(palindromes("teasdgrsgsdfAnt"));
 
 // Do not edit below this line
 module.exports = palindromes;
